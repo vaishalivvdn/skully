93f9ecefa8257d4aaf7c7470b11e7dabeab80ddc
vaishali <vaishali.ganesh@vvdntech.in>
2015-02-12 16:33:23 +0530

    .

--- new_script.sh
+++ new_script.sh
@@ -4,7 +4,7 @@ git status
 git remote add skully https://github.com/vaishalivvdn/skully.git 
 git clone https://github.com/vaishalivvdn/skully.git 
 git commit -am .
-git pull skully master 
+git pull skully omap4_usb_boot 
 
 git remote add gerrit http://192.168.101.20:8080/
 git clone http://192.168.101.20:8080/
@@ -12,4 +12,4 @@ git commit -am .
 git push gerrit master
 
 git pull gerrit master
-git push skully master
+git push skully omap4_usb_boot

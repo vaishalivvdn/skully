56cd0dffe1ef4359ea3bc5d42bf3922ee497b608
vaishali <vaishali.ganesh@vvdntech.in>
2015-02-12 16:31:20 +0530

    .

--- new_script.sh
+++ new_script.sh
@@ -2,9 +2,13 @@ git init
 git status
 
 git remote add skully https://github.com/vaishalivvdn/skully.git 
+git clone https://github.com/vaishalivvdn/skully.git 
+git commit -am .
 git pull skully master 
 
 git remote add gerrit http://192.168.101.20:8080/
+git clone http://192.168.101.20:8080/
+git commit -am .
 git push gerrit master
 
 git pull gerrit master
